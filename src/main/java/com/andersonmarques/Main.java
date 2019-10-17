24a34d5b9e7dd214ab3698ba32bbd77ac71259bb
unknown <andersongabriel96.ag@gmail.com>
2019-10-17 14:56:04 -0300

    Adicionado funcionalidade de abrir browser na página inicial do projeto

--- src/main/java/com/andersonmarques/Main.java
+++ src/main/java/com/andersonmarques/Main.java
@@ -1,12 +1,34 @@
 package com.andersonmarques;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Main.class, args);
+		abrirBrowser();
+	}
+
+	/**
+	 * Método responsável por executar comandos no terminal, de acordo com o sistema
+	 * operacional em que a JVM está rodando.
+	 * 
+	 * @author https://www.baeldung.com/run-shell-command-in-java
+	 * @throws Exception
+	 */
+	private static void abrirBrowser() throws Exception {
+		boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+		ProcessBuilder builder = new ProcessBuilder();
+		System.out.println("Tentando iniciar browser e abrir página localhost:8081");
+		if (isWindows) {
+			builder.command("cmd.exe", "/c", "start http://localhost:8081");
+		} else {
+			builder.command("sh", "-c", "google-chrome http://localhost:8081");
+		}
+		builder.directory(new File(System.getProperty("user.home"))).start();
 	}
 }
